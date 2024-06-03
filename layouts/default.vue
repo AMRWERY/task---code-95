2b7e29348b4141dafce1f3df3b8d4765e6719156
AMRWERY <amrmounir2@gmail.com>
2024-06-04 00:41:18 +0300

    ..

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <navbar />
-    <slot />
+    <main>
+      <div class="max-w-full min-h-screen px-2 py-2 mx-auto sm:px-6 lg:px-14">
+        <slot />
+      </div>
+    </main>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-2xl font-semibold text-center">welcome to Code-95 😁</h1>
+    <ranges />
   </div>
 </template>
 
